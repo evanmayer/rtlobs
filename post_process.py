4d3f8573cbeea4e194bc97589d7c92cd34629144
evanmayer <evanmayer@uchicago.edu>
2019-11-03 19:38:24 -0700

    Implemented frequency-switched obervation post-processing function.

--- post_process.py
+++ post_process.py
@@ -42,170 +42,54 @@ def plot_spectrum(freqs, powers, savefig=None):
     return fig
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# by Evan Mayer
-
-import sys
-# Import plotting necessities
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def plot_file(ax, filename):
+def f_throw_fold(freqs_on, freqs_off, p_on, p_off):
     '''
-    Parse contents of filename, ignoring any lines that dont start with a float
+    Perform a "folding" technique on two spectra with the target signal present
+    in order to nearly completely subtract off baseline variation and increase
+    singal-to-noise.
+    As a matter of convention, I have assumed that the spectrum centered on the
+    frequency of interest is freqs_on, p_on. This doesn't matter, up to a
+    minus sign in the folded spectrum y-vals.
+    It is assumed that all input iterables have matching length.
+
+    This is meant to implement the method described here:
+    http://herschel.esac.esa.int/hcss-doc-15.0/load/hifi_um/html/hcb_pfsw.html 
+
+    Inputs:
+    freqs_on:   Array of frequencies in power spectrum, taken at the target 
+                frequency
+    freqs_off:  Array of frequencies in power spectrum, taken shifted off of
+                the target frequency. For "in-band" switching mode, this
+                should still contain the spectral line of interest.
+
+    p_on:       Array of power spectral density estimates (assumed dB/Hz 
+                uncalibrated), taken at the target frequency
+    p_off:      Array of power spectral density estimates (assumed dB/Hz 
+                uncalibrated), taken shifted off of the target frequency.
+
+    Returns:
+    freqs_fold: Array of frequencies at which the two switched spectra overlap
+    p_fold:     Folded PSD spectrum
     '''
-    freqs, powers = np.loadtxt(filename, dtype=float, unpack=True, skiprows=5, comments='#',
-        delimiter=' ')
 
-    freqs_ghz = freqs / 1.e9
-    ax.plot(freqs_ghz, powers, label=filename)
-    plt.locator_params(axis='x', nbins=9)
-    plt.locator_params(axis='y', nbins=12)
-    # ax.set_ylim([-50, -30])
-    ax.set_xlabel('Frequency (GHz)')
-    ax.set_ylabel('Power Spectral Density (dB/Hz)')
-    plt.title('Power Spectral Density Estimate')
+    # Shift-and-add to fold
+    # Find the center frequency in freqs_on
+    fc = freqs_on[len(freqs_on)//2]
+    # Magnitude of the frequency switch in frequency space
+    fthrow = abs(freqs_on[0] - freqs_off[0])
+    # Magnitude of frequency shift in bin space:
+    epsilon = (freqs_on[1] - freqs_on[0]) / 2.
+    fc_idx = np.where(np.abs(freqs_on - fc) < epsilon)[0][0]
+    fthrow_idx = np.where(np.abs(freqs_on - (fc-fthrow)) < epsilon)[0][0]
+    bin_throw = np.abs(fthrow_idx - fc_idx)
 
-    return
+    # Folding procedure is a shift-and-add-negate, then average
+    p_diff = p_on - p_off
+    # Average the spectrum of interest with the spectrum off by fthrow
+    p_fold = (p_diff[bin_throw:-1] - p_diff[0:len(p_diff)-bin_throw-1]) / 2.
+    # Get the folded, upper segment of freqs to return it, throwing away the 
+    # part the two spectra did not have in common.
+    freqs_fold = freqs_on[bin_throw:-1]
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.exit("Expected at least one filename as an argument.")
+    return freqs_fold, p_fold
 
-    # handle inputs
-    filenames = sys.argv[1:]
-
-    for i, file in enumerate(filenames):
-        plt.figure(1)
-        ax = plt.gca()
-        plot_file(ax, file)
-        plt.legend()
-    ax.grid()
-    plt.show()
-    plt.close()
