fb8e78e30f0aadbc51479fb003dd40f24189e99f
evanmayer <evanmayer@uchicago.edu>
2019-12-01 15:22:48 -0700

    Naive frequency switching mode implemented. Heavily handicapped by time to retune on each cycle, but functional.

--- collect.py
+++ collect.py
@@ -128,7 +128,7 @@ def run_spectrum_int( NFFT, gain, rate, fc, t_int ):
     gain:     Requested SDR gain (dB)
     rate:     SDR sample rate, intrinsically tied to bandwidth in SDRs (Hz)
     fc:       Base center frequency (Hz)
-    t_int:    Total integration time (s)
+    t_int:    Total effective integration time (s)
 
     Returns:
     freqs:    Frequencies of the resulting spectrum, centered at fc (Hz), numpy array
@@ -180,8 +180,6 @@ def run_spectrum_int( NFFT, gain, rate, fc, t_int ):
         for cnt in range(num_loops):
             iq = sdr.read_samples(NFFT)
             
-            #p_xx, freqs = psd(iq, NFFT=NFFT, Fs=rate, scale_by_freq=False)
-
             freqs, p_xx = welch(iq, fs=rate, nperseg=NFFT, noverlap=0, scaling='spectrum', return_onesided=False)
             p_xx_tot += p_xx
         
@@ -213,8 +211,6 @@ def run_spectrum_int( NFFT, gain, rate, fc, t_int ):
         # Shift frequency spectra back to the intended range
         freqs = freqs + fc
 
-        #save_spectrum('p_avg.txt', freqs, p_avg)
-
         # nice and tidy
         sdr.close()
 
@@ -228,6 +224,122 @@ def run_spectrum_int( NFFT, gain, rate, fc, t_int ):
         sdr.close()
 
     return freqs, p_avg
+
+
+def run_fswitch_int( NFFT, gain, rate, fc, fthrow, t_int ):
+    '''
+    Inputs:
+    NFFT:     Number of elements to sample from the SDR IQ timeseries: powers of 2 are most efficient
+    gain:     Requested SDR gain (dB)
+    rate:     SDR sample rate, intrinsically tied to bandwidth in SDRs (Hz)
+    fc:       Base center frequency (Hz)
+    fthrow:   Alternate frequency (Hz)
+    t_int:    Total effective integration time (s)
+
+    Returns:
+    freqs_fold: Frequencies of the spectrum resulting from folding according to the folding method implemented in the f_throw_fold (post_process module)
+    p_fold:     Folded frequency-switched power spectral density, centered at fc,(dB/Hz) numpy array.
+    '''
+    from scipy.signal import welch
+    from post_process import f_throw_fold 
+    import rtlsdr.helpers as helpers
+
+    sdr = RtlSdr()
+
+    try:
+        sdr.rs = rate # Rate of Sampling (intrinsically tied to bandwidth with SDR dongles)
+        sdr.fc = fc
+        sdr.gain = gain
+        print('  sample rate: %0.6f MHz' % (sdr.rs/1e6))
+        print('  center frequency %0.6f MHz' % (sdr.fc/1e6))
+        print('  gain: %d dB' % sdr.gain)
+        print('  num samples per call: {}'.format(NFFT))
+        print('  requested integration time: {}s'.format(t_int))
+        N = int(sdr.rs * t_int)
+        num_loops = (N//NFFT)+1
+        print('  => num samples to collect: {}'.format(N))
+        print('  => est. num of calls: {}'.format(num_loops))
+
+        # Set up arrays to store power spectrum calculated from I-Q samples
+        freqs_on = np.zeros(NFFT)
+        freqs_off = np.zeros(NFFT)
+        p_xx_on = np.zeros(NFFT)
+        p_xx_off = np.zeros(NFFT)
+        cnt = 0
+
+        # Set the baseline time
+        start_time = time.time()
+        print('Integration began at {}'.format(time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime(start_time))))
+
+        # Swap between the two specified frequencies, integrating signal.
+        # Time integration loop
+        for cnt in range(num_loops):
+            tick = (cnt%2 == 0)
+            if tick:
+                sdr.fc = fc
+            else:
+                sdr.fc = fthrow
+
+            iq = sdr.read_samples(NFFT)
+
+            if tick:
+                freqs_on, p_xx = welch(iq, fs=rate, nperseg=NFFT, noverlap=0, scaling='spectrum', return_onesided=False)
+                p_xx_on += p_xx
+            else:
+                freqs_off, p_xx = welch(iq, fs=rate, nperseg=NFFT, noverlap=0, scaling='spectrum', return_onesided=False)
+                p_xx_off += p_xx
+        
+        end_time = time.time()
+        print('Integration ended at {} after {} seconds.'.format(time.strftime('%a, %d %b %Y %H:%M:%S'), end_time-start_time))
+        print('{} spectra were measured, split between {} and {}.'.format(cnt, fc, fthrow))
+        print('for an effective integration time of {:.2f}s'.format(NFFT * cnt / rate))
+
+        half_len = len(freqs_on)//2
+        # Swap frequencies:
+        tmp_first = freqs_on[:half_len].copy() 
+        tmp_last = freqs_on[half_len:].copy()
+        freqs_on[:half_len] = tmp_last
+        freqs_on[half_len:] = tmp_first
+
+        tmp_first = freqs_off[:half_len].copy() 
+        tmp_last = freqs_off[half_len:].copy()
+        freqs_off[:half_len] = tmp_last
+        freqs_off[half_len:] = tmp_first
+
+        # Swap powers:
+        tmp_first = p_xx_on[:half_len].copy()
+        tmp_last = p_xx_on[half_len:].copy()
+        p_xx_on[:half_len] = tmp_last
+        p_xx_on[half_len:] = tmp_first
+
+        tmp_first = p_xx_off[:half_len].copy()
+        tmp_last = p_xx_off[half_len:].copy()
+        p_xx_off[:half_len] = tmp_last
+        p_xx_off[half_len:] = tmp_first
+
+        # Compute the average power spectrum based on the number of spectra read
+        p_avg_on  = 10.*np.log10(p_xx_on  / cnt)
+        p_avg_off = 10.*np.log10(p_xx_off / cnt)
+        # Shift frequency spectra back to the intended range
+        freqs_on = freqs_on + fc
+        freqs_off = freqs_off + fthrow
+
+        # Fold switched spectra
+        freqs_fold, p_fold = f_throw_fold(freqs_on, freqs_off, p_avg_on, p_avg_off)
+
+        # nice and tidy
+        sdr.close()
+
+    except OSError as err:
+        print("OS error: {0}".format(err))
+        raise(err)
+    except:
+        print('Unexpected error:', sys.exc_info()[0])
+        raise
+    finally:
+        sdr.close()
+
+    return freqs_fold, p_fold
 
 
 def save_spectrum(filename, freqs, p_xx):
