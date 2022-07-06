f2716333dbff831e502945fa6b6c63943fc6e15a
Evan Mayer <evanmayer@uchicago.edu>
2022-07-05 21:30:04 -0700

    passthrough of rtl_biast generic GPIO control

--- rtlobs/utils.py
+++ rtlobs/utils.py
@@ -4,11 +4,10 @@ by Evan Mayer
 Library for common functions used by an rtl-sdr based radio telescope.
 '''
 
-import os
 import subprocess
 
 
-def biast(state: int, index=0):
+def biast(state: int, index=0, gpio=0):
     '''
     Turn the bias tee on the device on or off.
     You should configure the path here to call the rtl_biast
@@ -20,6 +19,13 @@ def biast(state: int, index=0):
         nonzero = on, zero = off
     index (int)(optional)
         RTL-SDR device index, in the event of more than one SDR
+    gpio (int)(optional)
+        The rtl_biast utility allows access to RTL2832U GPIO outputs. The
+        RTL-SDRblog v3 dongle has these broken out on the board:
+        5: RTL-SDRblog breakout header 30
+        4: RTL-SDRblog breakout header 31
+        2: RTL-SDRblog breakout header 32
+        0: RTL2832U pin 37 (used for RTL-SDRblog v3 bias-tee)
     '''
 
     if state != 0:
@@ -28,10 +34,8 @@ def biast(state: int, index=0):
     else:
         print('Disabling bias tee.')
 
-    # Ensure the bias tee is turned off, then turn it on.
-    basepath = os.path.expanduser('~/github/')
-    cmd = [os.path.join(basepath, 'rtl-sdr-blog', 'build', 'src', 'rtl_biast'),  '-d {}'.format(index), '-b {}'.format(state)]
-    ret = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)#, text=True)
+    cmd = 'rtl_biast -d {} -b {} -g {}'.format(index, state, gpio).split()
+    ret = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     if ret.returncode == 0:
         print(ret.stdout)
